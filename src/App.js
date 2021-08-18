80a6d447b83cd39559a2f7858383b80b32d2d3e6
Janley Crsiologo <crisoja@oocl.com>
2021-08-18 20:51:48 +0800

    [TodoList][Janley] - feat: install react router and implement BrowserRouter,Switch and Link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import TodoList from "./features/todos/components/TodoList";
+import {Route, Link, BrowserRouter, Switch} from "react-router-dom";
+import DoneList from "./features/todos/components/DoneList";
+import PageNotFound from "./features/todos/components/PageNotFound";
+
 
 function App() {
   return (
+
+    
     <div className="App">
       <TodoList />
+      <BrowserRouter>
+                <ul>
+                    <Link to="/">go to todo list page</Link>
+                    <Link to="/done">go to done list page</Link>
+                </ul>
+                <Switch>
+                    <Route exact path="/" component={TodoList}></Route>
+                    <Route exact path="/done" component={DoneList}></Route>
+                    <Route path="*" component={PageNotFound}></Route>
+                </Switch>
+            </BrowserRouter>
     </div>
   );
 }
---
+++ src/features/todos/components/DoneList.js
@@ -0,0 +1,5 @@
+function DoneList() {
+
+}
+
+export default DoneList; 
